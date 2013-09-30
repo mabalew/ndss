83c789c83488410106546d4314e90a7bba5cf77c
Mariusz Balewski <mariusz@balewski.eu>
2013-09-30 19:54:27 +0200

    NetPropertiesClient renamed to NDSClient.

--- server/src/main/webapp/NDSClient.java
+++ server/src/main/webapp/NDSClient.java
@@ -11,21 +11,29 @@ import org.apache.log4j.Logger;
 /**
  * @author: Mariusz Balewski
  */
-public class NetPropertiesClient {
+public class NDSClient {
 
-	private final static Logger LOGGER = Logger.getLogger(NetPropertiesClient.class);
+	private final static Logger LOGGER = Logger.getLogger(NDSClient.class);
 
 	private String propertyName;
 	private String propertyValue;
 	private String sgid;
-	private String operation;
+	private Operation operation;
 
-	public NetPropertiesClient(String operation, String propertyName, String propertyValue, String sgid) {
+  public enum Operation {
+    ADD,
+    UPDATE,
+    GET,
+    LIST,
+    DELETE;
+  };
+
+	public NDSClient(Operation operation, String propertyName, String propertyValue, String sgid) {
 		this(operation, propertyName, sgid);
 		this.propertyValue = propertyValue;
 	}
 
-	public NetPropertiesClient(String operation, String propertyName, String sgid) {
+	public NDSClient(Operation operation, String propertyName, String sgid) {
 		this.operation = operation;
 		this.propertyName = propertyName;
 		this.sgid = sgid;
@@ -33,7 +41,7 @@ public class NetPropertiesClient {
 
 	public String call() {
 		Map<String, String> params = new LinkedHashMap<String, String>();
-		params.put("operation", this.operation);
+		params.put("operation", this.operation.toString());
 		params.put("propertyName", this.propertyName);
 		params.put("propertyValue", this.propertyValue);
 		params.put("sgid", this.sgid);
